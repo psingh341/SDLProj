f0635a250777c93c41d303c555df707778c208a3
Prajesh Singh <psingh8@ncsu.edu>
2017-06-05 09:41:16 -0400

    Fixing GUI

--- SDLProject/src/GUI.java
+++ SDLProject/src/GUI.java
@@ -55,7 +55,7 @@ public class GUI {
 
 		// Initial panel that is presented at the start of the program
 		JPanel InitView = new JPanel();
-		InitView.setBackground(new Color(204, 204, 153));
+		InitView.setBackground(SystemColor.menu);
 		frame.getContentPane().add(InitView, "name_933255140938789");
 		InitView.setLayout(null);
 
@@ -96,6 +96,9 @@ public class GUI {
 		lblWhatIsThe.setFont(new Font("Times New Roman", Font.PLAIN, 15));
 		lblWhatIsThe.setBounds(38, 249, 332, 23);
 		InitView.add(lblWhatIsThe);
+		
+		JPanel UnixHWConfig = new JPanel();
+		frame.getContentPane().add(UnixHWConfig, "name_149004331042821");
 
 		// Button on UnixHWConfig page responsible for linking to module website
 		final URI uriOne = new URI("http://share.allstate.com/sites/bhs/shs/Modules/Forms/Allltems.aspx");
@@ -104,177 +107,6 @@ public class GUI {
 			public void actionPerformed(ActionEvent e) {
 			}
 		}
-
-		// The server count for opod
-		String[] numServersOPOD = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
-
-		// Environment comboBox for OPOD
-		String[] environmentsOPOD = { "", "Production", "Pre-Production", "Training", "Continuity", "eTest", "Test", "Development",
-									  "Lab", "TPT"};
-
-		// Combo Box for OPOD Buis Units
-		String[] buisUnitsOPOD = { "", "Affinity", "Agency Distribution - Sales", "ALMCO", "Allstate Benefits",
-				"Allstate Buisness Insurance (Commerical)", "Allstate Dealer Services(Credit Division)",
-				"Allstate Finance Company", "Allstate Financial Technology", "Allstate Independent Agents",
-				"Allstate Investments", "Allstate Roadside Services (ARS)", "Allstate Solutions Private Limited (ASPL)",
-				"Allstate Vehicle & Property(AVPIC)", "ANI", "ANJ", "Architecture & Simplification", "ASAP", "ASIC",
-				"ATO - PROD OPS", "ATO Finance", "ATO Strategy", "B2B Technology", "Broker Dealer", "Canada", "CEO COO",
-				"CIC", "Claims", "Claims Technology Services", "Connected Car Technologies", "Consumer Household(SPL)",
-				"Corporate Relations", "Customer Experience & Retention", "Dealer Lab", "Delivery & Risk Management",
-				"Direct Channel of Bind - CIC", "Direct Channel of Bind - Internet",
-				"DRM - Buisness/Application Architecture", "DRM - Enterprise Testing & Release Mgmt",
-				"DRM - Information Security", "DRM - Program Management Office", "DRM - Risk Mgmt & Compliance",
-				"Encompass", "Encompass Claims", "Encompass Support", "Enterprise", "Enterprise Shared Services",
-				"Esurance", "Facility Services", "Finance", "Human Resources", "Information Services Group",
-				"Infrastructure Services", "Investments Technology", "IS - ANI IS",
-				"IS - Client & Middleware Platforms", "IS - Foundation Platforms", "IS - Infrastructure Operations",
-				"IS - Network Services", "IS - Product Operations", "IS - Production Processes",
-				"IS - Server Platforms", "IS - Solutions Enginerring & Service Mgmt",
-				"ISG - Business Intelligence & Analytics", "ISG - Customer Data Strategy & Data Quality",
-				"ISG - Data Solutions", "ISG - Database Administrator", "ISG - ICS/Customer Data Strategy",
-				"ISG - Intgrated Customer Services", "IVANTAGE", "Kennet", "Law & Reg Claim Litigation",
-				"Law & Regulation", "Life", "Marketing", "Motor Club", "North Light", "Northbrook Services",
-				"Operations", "Ops - ATO Effectiveness & Engagement", "Ops - BPM & Quality",
-				"Ops - Business Process Competency Center", "Ops - Business Process Delivery",
-				"Ops - Enterprise Support", "Ops - Global Delivery Integration & Sourcing", "Ops - Operations CIO",
-				"Ops - Policy Administration", "Ops - Processing Support for Claims",
-				"Ops - Selling Policies & Agency Support", "Partnership Marketing  Group (PMG)", "Personal Lines",
-				"Personal Lines - California", "Personal Lines(Exc.ANJ)", "Personal Lines(Incl.ANJ)",
-				"Protection Risk & Return Mgmt", "Real Estate & Construction", "Research Center", "RMBC",
-				"Sales, Mktg & Customer Experience", "SMCE - Agency Facing", "SMCE - Customer Facing",
-				"Sourcing & Procurement Solutions", "Specialty Ops", "Sterling", "Techcor", "Technology Solutions" };
-
-		// Data center options for OPOD
-		String[] opsOPOD = { "", "Hudson", "Rochelle" };
-
-		// Enterprise Options combo box for OPOD
-		String[] entOpsOPOD = { "None", "sxm0029", "sxm0034" };
-
-		// Wing location Options for OPOD
-		String[] wingOpsOPOD = { "", "East", "West" };
-		
-		// combo box for vpool storage array in OPOD page
-		String[] vpoolOpts = {"", "Storage_Subsystem_1001446", "Storage_Subsystem_1001646", "Storage_Subsystem_1099", 
-							  "Storage_Subsystem_1201999", "Storage_Subsystem_1415", "Storage_Subsystem_17335", "Storage_Subsystem_28573",
-							  "Storage_Subsystem_4547", "Storage_Subsystem_48504", "Storage_Subsystem_4961", "Storage_Subsystem_5018",
-							  "Storage_Subsystem_54071", "Storage_Subsystem_55290", "Storage_Subsystem_55300", "Storage_Subsystem_55401",
-							  "Storage_Subsystem_65197", "Storage_Subsystem_65278", "Storage_Subsystem_65284", "Storage_Subsystem_65301",
-							  "Storage_Subsystem_65302", "Storage_Subsystem_65383", "Storage_Subsystem_65579", "Storage_Subsystem_66539",
-							  "Storage_Subsystem_66796", "Storage_Subsystem_66804", "Storage_Subsystem_66897"};
-		
-		// number of databases combo box in OPOD page
-		String[] numDatabasesOpts = {"1", "2", "3", "4", "5", "6", "7", "8"};
-		
-		// Combo box for number of disk groups in OPOD page
-		String[] numDiskGroupsOpts = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"};
-
-		// The server count for hadoop
-		String[] numServers1 = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
-
-		// Environment comboBox for Hadoop
-		String[] environments1 = { "", "Production", "Pre-Pro", "QA", "I-Test", "E-Test", "A-Test" };
-
-		// Combo Box for Hadoop Buis Units
-		String[] buisUnits1 = { "", "Affinity", "Agency Distribution - Sales", "ALMCO", "Allstate Benefits",
-				"Allstate Buisness Insurance (Commerical)", "Allstate Dealer Services(Credit Division)",
-				"Allstate Finance Company", "Allstate Financial Technology", "Allstate Independent Agents",
-				"Allstate Investments", "Allstate Roadside Services (ARS)", "Allstate Solutions Private Limited (ASPL)",
-				"Allstate Vehicle & Property(AVPIC)", "ANI", "ANJ", "Architecture & Simplification", "ASAP", "ASIC",
-				"ATO - PROD OPS", "ATO Finance", "ATO Strategy", "B2B Technology", "Broker Dealer", "Canada", "CEO COO",
-				"CIC", "Claims", "Claims Technology Services", "Connected Car Technologies", "Consumer Household(SPL)",
-				"Corporate Relations", "Customer Experience & Retention", "Dealer Lab", "Delivery & Risk Management",
-				"Direct Channel of Bind - CIC", "Direct Channel of Bind - Internet",
-				"DRM - Buisness/Application Architecture", "DRM - Enterprise Testing & Release Mgmt",
-				"DRM - Information Security", "DRM - Program Management Office", "DRM - Risk Mgmt & Compliance",
-				"Encompass", "Encompass Claims", "Encompass Support", "Enterprise", "Enterprise Shared Services",
-				"Esurance", "Facility Services", "Finance", "Human Resources", "Information Services Group",
-				"Infrastructure Services", "Investments Technology", "IS - ANI IS",
-				"IS - Client & Middleware Platforms", "IS - Foundation Platforms", "IS - Infrastructure Operations",
-				"IS - Network Services", "IS - Product Operations", "IS - Production Processes",
-				"IS - Server Platforms", "IS - Solutions Enginerring & Service Mgmt",
-				"ISG - Business Intelligence & Analytics", "ISG - Customer Data Strategy & Data Quality",
-				"ISG - Data Solutions", "ISG - Database Administrator", "ISG - ICS/Customer Data Strategy",
-				"ISG - Intgrated Customer Services", "IVANTAGE", "Kennet", "Law & Reg Claim Litigation",
-				"Law & Regulation", "Life", "Marketing", "Motor Club", "North Light", "Northbrook Services",
-				"Operations", "Ops - ATO Effectiveness & Engagement", "Ops - BPM & Quality",
-				"Ops - Business Process Competency Center", "Ops - Business Process Delivery",
-				"Ops - Enterprise Support", "Ops - Global Delivery Integration & Sourcing", "Ops - Operations CIO",
-				"Ops - Policy Administration", "Ops - Processing Support for Claims",
-				"Ops - Selling Policies & Agency Support", "Partnership Marketing  Group (PMG)", "Personal Lines",
-				"Personal Lines - California", "Personal Lines(Exc.ANJ)", "Personal Lines(Incl.ANJ)",
-				"Protection Risk & Return Mgmt", "Real Estate & Construction", "Research Center", "RMBC",
-				"Sales, Mktg & Customer Experience", "SMCE - Agency Facing", "SMCE - Customer Facing",
-				"Sourcing & Procurement Solutions", "Specialty Ops", "Sterling", "Techcor", "Technology Solutions" };
-
-		// Data center options for Hadoop
-		String[] ops1 = { "", "Hudson", "Rochelle" };
-
-		// Enterprise Options combo box for Hadoop
-		String[] entOps1 = { "None", "sxm0029", "sxm0034" };
-
-		// Wing location Options for Hadoop
-		String[] wingOps = { "", "East", "West" };
-
-		// Jcombo box for the number of servers for TIBCO
-		String[] numServers = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
-
-		// Environment options jcombo box for TIBCO
-		String[] environments = { "", "Production", "Pre-Pro", "QA", "I-Test", "E-Test", "A-Test" };
-
-		// Combo Box for business units for TIBCO
-		String[] buisUnits = { "Technology Solutions", "Affinity", "Agency Distribution - Sales", "ALMCO",
-				"Allstate Benefits", "Allstate Buisness Insurance (Commerical)",
-				"Allstate Dealer Services(Credit Division)", "Allstate Finance Company",
-				"Allstate Financial Technology", "Allstate Independent Agents", "Allstate Investments",
-				"Allstate Roadside Services (ARS)", "Allstate Solutions Private Limited (ASPL)",
-				"Allstate Vehicle & Property(AVPIC)", "ANI", "ANJ", "Architecture & Simplification", "ASAP", "ASIC",
-				"ATO - PROD OPS", "ATO Finance", "ATO Strategy", "B2B Technology", "Broker Dealer", "Canada", "CEO COO",
-				"CIC", "Claims", "Claims Technology Services", "Connected Car Technologies", "Consumer Household(SPL)",
-				"Corporate Relations", "Customer Experience & Retention", "Dealer Lab", "Delivery & Risk Management",
-				"Direct Channel of Bind - CIC", "Direct Channel of Bind - Internet",
-				"DRM - Buisness/Application Architecture", "DRM - Enterprise Testing & Release Mgmt",
-				"DRM - Information Security", "DRM - Program Management Office", "DRM - Risk Mgmt & Compliance",
-				"Encompass", "Encompass Claims", "Encompass Support", "Enterprise", "Enterprise Shared Services",
-				"Esurance", "Facility Services", "Finance", "Human Resources", "Information Services Group",
-				"Infrastructure Services", "Investments Technology", "IS - ANI IS",
-				"IS - Client & Middleware Platforms", "IS - Foundation Platforms", "IS - Infrastructure Operations",
-				"IS - Network Services", "IS - Product Operations", "IS - Production Processes",
-				"IS - Server Platforms", "IS - Solutions Enginerring & Service Mgmt",
-				"ISG - Business Intelligence & Analytics", "ISG - Customer Data Strategy & Data Quality",
-				"ISG - Data Solutions", "ISG - Database Administrator", "ISG - ICS/Customer Data Strategy",
-				"ISG - Intgrated Customer Services", "IVANTAGE", "Kennet", "Law & Reg Claim Litigation",
-				"Law & Regulation", "Life", "Marketing", "Motor Club", "North Light", "Northbrook Services",
-				"Operations", "Ops - ATO Effectiveness & Engagement", "Ops - BPM & Quality",
-				"Ops - Business Process Competency Center", "Ops - Business Process Delivery",
-				"Ops - Enterprise Support", "Ops - Global Delivery Integration & Sourcing", "Ops - Operations CIO",
-				"Ops - Policy Administration", "Ops - Processing Support for Claims",
-				"Ops - Selling Policies & Agency Support", "Partnership Marketing  Group (PMG)", "Personal Lines",
-				"Personal Lines - California", "Personal Lines(Exc.ANJ)", "Personal Lines(Incl.ANJ)",
-				"Protection Risk & Return Mgmt", "Real Estate & Construction", "Research Center", "RMBC",
-				"Sales, Mktg & Customer Experience", "SMCE - Agency Facing", "SMCE - Customer Facing",
-				"Sourcing & Procurement Solutions", "Specialty Ops", "Sterling", "Techcor" };
-
-		// Data center combo box for TIBCO page
-		String[] ops = { "", "Hudson", "Rochelle" };
-
-		// Combo Box for Wing Location TIBCO page
-		String[] locationOpts = { "", "East", "West" };
-
-		// Enterprise controller comboBox
-		String[] entOps = { "None", "sxm0029", "sxm0034" };
-
-		// Security Ops combobox for TIBCO page
-		String[] securityOps = { "", "MPN_Other_0", "DMZ_AccessMgmt_0", "DMZ_AccessMgmt_0_Pub", "DMZ_Vendor_0",
-				"DMZ_Vendor_0_Pub", "N/A(for Tier 0 Servers" };
-
-		// Behavior combo box for TIBCO
-		String[] behaviors = { "", "Active/Active", "Swappable" };
-
-		// Module options combobox for TIBCO
-		String[] moduleOps = { "", "Business", "EMS", "Other" };
-
-		// Maintenance options combobox for TIBCO
-		String[] maintainOps = { "", "M4", "M5" };
 
 		// Adds the selection of choices after user selects Windows
 		windowsButton.addActionListener(new ActionListener() {
